6ed611912d0116408d83385108975f1f49883292
Arun-Jawlia <arunkumar08.mk@gmail.com>
2023-11-23 10:19:37 +0530

    install react app using vite and add basic structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        App
+        </div>
+       
+    </>
+  )
+}
+
+export default App
