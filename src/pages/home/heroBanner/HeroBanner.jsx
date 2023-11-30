a7f919ec863366abbb6aa9abec1edc1696f260c9
Arun-Jawlia <101392646+Arun-Jawlia@users.noreply.github.com>
2023-11-30 19:13:34 +0530

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
 import Img from "../../../components/lazyLoadImage/Img";
-import ContentWrapper from "../../../components/ContentWrapper/ContentWrapper";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
